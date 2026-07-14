87439b08f2580f7533b28262088205c975e1fb29
Vinayak Ritti <rittivinayak.1@gmail.com>
2026-07-15 00:33:19 +0530

    Restored flat folder structure and retained Employee changes

--- Desktop/Valaxy/Hands-on Coding Projects/springBootProject/src/main/java/com/example/demo_jpa_hibernate/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo_jpa_hibernate.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
